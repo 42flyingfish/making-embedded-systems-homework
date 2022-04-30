0ac7d9356fbb0a32f2991185c7df9507ec978ccd
Eric <cire.ttalp@gmail.com>
2022-04-29 23:55:16 -0700

    forgot main

--- week5/src/main.c
+++ week5/src/main.c
@@ -1,10 +1,22 @@
 #include "pico/stdlib.h"
+#include "console.h"
 
 int main() {
-    stdio_init_all();
+    //stdio_init_all();
+    /*
+     * Sets up default urart
+     * TX on GPIO 0
+     * RX on GPIO 1
+     * UART 0
+     * baudrate 115200
+     */
+
+    setup_default_uart();
+    ConsoleInit();
 
     while (true) {
-	    puts("Hello world");
-	    sleep_ms(5000);
+	    ConsoleProcess();
+	    //uart_puts(uart0, "What is this nonsense\n\r");
+	    sleep_ms(2);
     }
 }
