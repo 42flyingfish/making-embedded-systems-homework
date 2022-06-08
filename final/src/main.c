69630fa88131ed8f283111dbde7b80f244a23a61
Eric <cire.ttalp@gmail.com>
2022-06-07 21:30:31 -0700

    Basic joystick support

--- final/src/main.c
+++ final/src/main.c
@@ -1,6 +1,9 @@
+#include "hardware/adc.h"
 #include "pico/stdlib.h"
 #include "console/console.h"
 #include "pico/st7789.h"
+#include "draw.h"
+#include "hardware/adc.h"
 
 
 const struct st7789_config lcd_config = {
@@ -18,14 +21,42 @@ const int lcd_height = 320;
 
 int main() {
 
+    /* ADC info
+    */
+    adc_init();
+    adc_gpio_init(26);
+    adc_gpio_init(27);
+
+
     ConsoleInit();
     st7789_init(&lcd_config, lcd_width, lcd_height);
-    st7789_fill(0xffff);
+    st7789_fill(0xf800);
 
 
 
     while (true) {
-	    ConsoleProcess();
-	    sleep_ms(2);
+        ConsoleProcess();
+	adc_select_input(0);
+        uint adc_x_raw = adc_read();
+        adc_select_input(1);
+        uint adc_y_raw = adc_read();
+	// directions
+	if (adc_x_raw < 0xf70) {
+		incrementCursorX();
+		drawPixel();
+	}
+	if (adc_x_raw > 0x010) {
+		decrementCursorX();
+		drawPixel();
+	}
+	if (adc_y_raw > 0xfa0) {
+		decrementCursorY();
+		drawPixel();
+	}
+	if (adc_y_raw < 0x010) {
+		incrementCursorY();
+		drawPixel();
+	}
+        sleep_ms(2);
     }
 }
